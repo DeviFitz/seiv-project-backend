7152d9a4869fc0b08b708e7673146bd5b5302a1f
Vyryty <colonelsuperawesome@gmail.com>
2024-02-06 15:25:40 -0600

    feat: Adding a few placeholder authenticators

--- app/routes/alert.routes.js
+++ app/routes/alert.routes.js
@@ -4,19 +4,19 @@ module.exports = (app) => {
     let router = require("express").Router();
   
     // Create a new Alert
-    router.post("/", [authenticate], alert.create);
+    router.post("/", [authenticate, /*Category Edit+*/], alert.create);
   
     // Retrieve all Alerts
-    router.get("/", [authenticate], alert.findAll);
+    router.get("/", [authenticate, /*Category Read+*/], alert.findAll);
   
     // Retrieve a single Alert with id
-    router.get("/:id", [authenticate], alert.findOne);
+    router.get("/:id", [authenticate, /*Category Read+*/], alert.findOne);
   
     // Update an Alert with id
-    router.put("/:id", [authenticate], alert.update);
+    router.put("/:id", [authenticate, /*Category Edit+*/], alert.update);
   
     // Delete an Alert with id
-    router.delete("/:id", [authenticate], alert.delete);
+    router.delete("/:id", [authenticate, /*Category Edit+*/], alert.delete);
   
     app.use("/asset-t3/alerts", router);
 };
